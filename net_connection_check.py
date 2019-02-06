67040d63b4e5dd7851129942e7aadd66084d13b6
root <root@DESKTOP-R2FE4CD.localdomain>
2019-02-06 18:26:25 -0500

    fixed crash when no default gateway found/NIC not connected

--- net_connection_check.py
+++ net_connection_check.py
@@ -6,32 +6,40 @@ import os
 
 remoteip = "8.8.8.8"
 hostname = "www.yahoo.com"
+connection = True
 
 os.system('clear')
 print('-' * 25)
 print "***Beginning Test***\n1\n2\n3\n4\n5\n"
 
-# get default gateway
-ipr = subprocess.check_output('ip route | grep default', shell=True)
-searchobj = re.search(r'(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})', ipr)
-gateway = searchobj.group()
-print("Your default gateway is " + gateway + "\n")
+try:
+	ipr = subprocess.check_output('ip route | grep default', shell=True)
+except:
+	connection = False	
 
-# ping the gateway
-if(os.system('ping -c 1 ' + gateway + ' > /dev/null')):
-	print "Gateway Connection FAILED\n"
+# get default gateway
+if(connection):
+	searchobj = re.search(r'(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})', ipr)
+	gateway = searchobj.group()
+	print("Your default gateway is " + gateway + "\n")
+
+	# ping the gateway
+	if(os.system('ping -c 1 ' + gateway + ' > /dev/null 2>/dev/null')):
+		print "Gateway Connection FAILED\n"
+	else:
+		print "Gateway Connection SUCCEEDED\n"
 else:
-	print "Gateway Connection SUCCEEDED\n"
+	print "No gateway to test\n"
 
 # ping the remote address
-if(os.system('ping -c 1 ' + remoteip + ' > /dev/null')):
-	print "Remote Connection FAILED'n"
+if(os.system('ping -c 1 ' + remoteip + ' > /dev/null 2>/dev/null')):
+	print "Remote Connection FAILED\n"
 else:
 	print "Remote Connection SUCCEEDED\n" 
 
 # ping the url to test DNS
-if(os.system('ping -c 1 ' + hostname + ' > /dev/null')):
-	print "DNS Resolution FAILED'n"
+if(os.system('ping -c 1 ' + hostname + ' > /dev/null 2>/dev/null')):
+	print "DNS Resolution FAILED\n"
 else:
 	print "DNS Resolution SUCCEEDED\n" 
 
